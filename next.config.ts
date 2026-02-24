0264bfa3ed82ebed3e82914b5a03489a801fcdef
Lisas Macbook <lisasmacbook@macbookpro.speedport.ip>
2026-02-24 22:25:01 +0100

    Fix Console Errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
+const csp = `
+  default-src 'self';
+  script-src 'self' https://challenges.cloudflare.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob:;
+  font-src 'self' data:;
+  connect-src 'self';
+  frame-src https://challenges.cloudflare.com;
+`.replace(/\s{2,}/g, " ").trim();
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: csp,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
