1a4299c3436cae9680b6d0ce9598e8348a6e5136
Lisas Macbook <lisasmacbook@Lisass-MacBook-Pro.local>
2026-04-03 13:45:11 +0200

    Add custom_page_view datalayer

--- components/custom-page-view-tracker.tsx
+++ components/custom-page-view-tracker.tsx
@@ -1,4 +1,3 @@
-// components/custom-page-view-tracker.tsx
 'use client'
 
 import { useEffect, useRef } from 'react'
@@ -7,8 +6,6 @@ import { usePathname, useSearchParams } from 'next/navigation'
 export default function CustomPageViewTracker() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
-
-  // Für den ersten Aufruf externer Referrer, danach interne vorige URL
   const previousUrlRef = useRef('')
 
   useEffect(() => {
@@ -25,12 +22,11 @@ export default function CustomPageViewTracker() {
     window.dataLayer = window.dataLayer || []
     window.dataLayer.push({
       event: 'custom_page_view',
-      page_path: pagePath,          // z.B. /web-analytics
-      page_location: currentUrl,    // volle aktuelle URL
+      page_path: pagePath,
+      page_location: currentUrl,
       page_referrer: previousUrlRef.current || '',
     })
 
-    // Für den nächsten SPA-Wechsel die aktuelle URL als Referrer merken
     previousUrlRef.current = currentUrl
   }, [pathname, searchParams])
 
